2e71c1c0c844de76257e9d50c301ea79c88f6837
Soto02 <asotfer636@g.educaand.es>
2025-05-26 22:19:55 +0200

    DELETE: filterPage y perfilPage
    He borrado la pagina de filtros ya que voy a usar un modal, y tambien he borrado la pagina de perfil

---
+++ src/app/Pages/profile/perfil.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonList,
+  IonLabel,
+} from '@ionic/angular/standalone';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.page.html',
+  styleUrls: ['./perfil.page.scss'],
+  standalone: true,
+  imports: [
+    IonLabel,
+    IonList,
+    IonItem,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    SharedModule,
+  ],
+})
+export class PerfilPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
